d976ba02d84850ca7d9b65b56d811fb63831a9ef
charlieabhishek <abcptech@gmail.com>
2019-07-01 20:00:28 +0530

    budgetctrl added.

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
+//BUDGET CONTROLLER
 var budgetController = (function(){
 
-    var x = 23;
+})();
 
-    var add = function(a){
-        return x + a;
-    }
-    
-    return {
-        publicTest: function(b){
-            console.log(add(b))
-        }
-    }
+// UI CONTROLLER
+var UIController = (function() {
+     
+    // Some Code
 
 })();
+
+//GLOBAL APP CONTROLLER
+var controller = (function(budgetCtrl, UICtrl){
+   document.querySelector(".add__btn").addEventListener('click', function(){
+      console.log('Button was clicked');
+   });
+})(budgetController, UIController);
